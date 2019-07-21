64abacf0df987caa578672165dd6c8e41d5e486e
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-07-21 15:59:23 +0200

    Adds description to listing (patch)

--- components/Candidates.js
+++ components/Candidates.js
@@ -7,12 +7,15 @@ const Candidates = () => {
       <div className='candidates'>
         <h2>Flere flotte kandidater</h2>
         {candidates.map(candidate => <p key={`candidate-${candidate.id}`}>
-          <Link href='/[cid]' as={`/${candidate.id}`}><a>{candidate.name}</a></Link></p>)}
+          <Link href='/[cid]' as={`/${candidate.id}`}><a>{candidate.name}</a></Link><br />{candidate.description}</p>)}
       </div>
       <style jsx global>
         {`
           .candidates {
             padding: 15px;
+          }
+          .candidates a {
+            font-weight: 700;
           }
           .candidates a, .candidates a:visited {
             color: #2e3c46;
